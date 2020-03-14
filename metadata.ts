58cff252a951f70617d050ee2a546aa7242f6b22
oconely <and.ribaltovsky@gmail.com>
2020-03-15 00:27:53 +0300

    added class and methods decorators

--- metadata.ts
+++ metadata.ts
@@ -13,4 +13,31 @@ const note = Reflect.getMetadata('note', plane);
 const height = Reflect.getMetadata('height', plane);
 const colorNote = Reflect.getMetadata('note', plane, 'color');
 
-console.log(note, height, colorNote);
+// console.log(note, height, colorNote);
+
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('Hi there!')
+  fly(): void {
+    console.log('vrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
